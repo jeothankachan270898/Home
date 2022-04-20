03cc795a4ae3e524b7bead4348aa8bcb993c9e70
jeothankachan270898 <jeothankachan270898@gmail.com>
2022-04-20 17:43:19 +0530

    initial commit

---
+++ constants/styles.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  primary100: '#D3D3D3',
+  primary200: '#000000',
+  primary500: '#6495ED',
+  primary800: '#ffffff',
+  error100: '#fcdcbf',
+  error500: '#f37c13',
+}
---
+++ screens/mainScreens/About.js
@@ -0,0 +1,11 @@
+import { View ,Text } from 'react-native'
+
+function About () {
+    return(
+        <View>
+            <Text>About Us</Text>
+        </View>
+    )
+
+}
+export default About
